ab4667e2d72d226bdf7acbe025d0e103d8f97b24
TJ Nine <teejten@Timothys-MBP.westell.com>
2016-09-06 15:00:48 -0400

    fix to Redirect
    Redirects work now

--- admin/profile.php
+++ admin/profile.php
@@ -1,16 +1,14 @@
 <?php 
 require_once '../core/init.php';
 
-if(!$username = Input::get('user')) {
-	Redirect::to('index.php');
-}else {
+
 	$user = new User($username);
 	if(!$user->exists()) {
 		Redirect::to(404);
 	}else {
 		$data = $user->data();
 	}
-}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
